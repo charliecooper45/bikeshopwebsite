c8228fb08a9d901687b557c2b181708f126d1702
charliecooper45 <charliecooper98@gmail.com>
2014-09-16 21:07:53 +0100

    Set up JNDI Datasource

--- BikeShopWebsite/src/controllers/HomeController.java
+++ BikeShopWebsite/src/controllers/HomeController.java
@@ -1,11 +1,20 @@
 package controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import beans.User;
 
@@ -15,12 +24,47 @@ import beans.User;
 @WebServlet("/HomeController")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private DataSource ds;
 
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		try {
+			InitialContext initContext = new InitialContext();
+			
+			Context env = (Context) initContext.lookup("java:comp/env");
+			
+			ds = (DataSource) env.lookup("jdbc/bikeshopdb");
+		} catch (NamingException e) {
+			throw new ServletException();
+		}
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String jspPage = null;
 		String action = request.getParameter("action");
 		
 		if(action == null) {
+			// Test database connection code
+			Connection conn = null;
+			
+			try {
+				conn = ds.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return;
+			}
+			
+			// use connection
+			PrintWriter out = response.getWriter();
+			out.println("Connected to database");
+			
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
 			jspPage = "/index.jsp";
 		} else {
 			switch(action) {
@@ -32,7 +76,7 @@ public class HomeController extends HttpServlet {
 			}
 		}
 		
-		getServletContext().getRequestDispatcher(jspPage).forward(request, response);
+		// getServletContext().getRequestDispatcher(jspPage).forward(request, response);
 	}
 	
 	@Override

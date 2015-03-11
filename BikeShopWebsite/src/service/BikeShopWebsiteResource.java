0c385edef2e7d98ea9018dbb99e35d388e41a83e
Charlie <charliecooper98@gmail.com>
2015-03-11 20:05:41 +0100

    Start of web services implementation

---
+++ BikeShopWebsite/src/service/BikeShopWebsiteResource.java
@@ -0,0 +1,17 @@
+package service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class BikeShopWebsiteResource {
+	@GET
+	@Produces({MediaType.TEXT_HTML})
+	public String getAllActivities() {
+		System.out.println("Get all activities!");
+		
+		return "<b>test</b>";
+	}
+}
